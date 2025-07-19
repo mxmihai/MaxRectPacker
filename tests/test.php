29896e259bf35e67259f0b22600f67b8d060ddb7
Mihai Marinescu <mihai@datagram.ro>
2025-07-19 23:28:11 +0300

    Create test.php

---
+++ tests/test.php
@@ -0,0 +1,11 @@
+---
+
+## 5. Test Script (`tests/test.php`)
+
+A simple test script to demonstrate usage:
+
+```php
+<?php
+require_once __DIR__ . '/../src/MaxRectPacker.php';
+
+MaxRectPacker::test();
